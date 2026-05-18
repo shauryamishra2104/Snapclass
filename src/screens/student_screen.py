6434f5cc9f542c8b033ba229fdb927046df5e84c
Shaurya_Mishra <atulmishra0504@gmail.com>
2026-05-18 21:59:30 +0530

    Voice and face recognition pipeline

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -3,6 +3,8 @@ import streamlit as st
 from src.ui.base_layout import style_background_dashboard,style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
+from PIL import Image
+import numpy as np
 
 def student_screen():
     style_background_dashboard()
@@ -20,8 +22,10 @@ def student_screen():
     st.space()
     st.space()
     
-    st.camera_input("Position your face in the Center")
-
+    photo_source = st.camera_input("Position your face in the Center")
+    
+    if photo_source:
+        np.array(Image.open(photo_source))
     footer_dashboard()
     
 student_screen()
